11b6c6f33e6b32208b8e6fb296d5fec758733cb8
MohsenDarabi <mohsen.darabi@outlook.de>
2019-07-19 22:30:16 +0200

    fixe pairs number to be checked from 1 to 8pairs

--- js/test2.js
+++ js/test2.js
@@ -92,7 +92,7 @@ function disableCards() {
 
   resetBoard();
   pairs++;
-  if(pairs===1){
+  if(pairs===8){
     let win=document.getElementById('welcome-screen');
     
     score.innerHTML='wel done';
