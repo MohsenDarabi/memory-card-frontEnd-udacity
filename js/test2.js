88431f21a1f36c7a8e7c49e4008628154f5b76c7
MohsenDarabi <mohsen.darabi@outlook.de>
2019-07-19 22:56:06 +0200

    Validate html and css

--- js/test2.js
+++ js/test2.js
@@ -1,10 +1,10 @@
-const cards = document.querySelectorAll('.memory-card');
-let pairs=0;
-let move=0;
-let score=document.getElementById('score');
+const cards = document.querySelectorAll(".memory-card");
+let pairs = 0;
+let move = 0;
+let score = document.getElementById("score");
 let turned = false;
 let doneCard = false;
-let timeCount = '';
+let timeCount = "";
 let minuteCount = 0;
 let secondsCount = 0;
 let firstCard, secondCard;
@@ -15,15 +15,14 @@ function select(element) {
   return document.querySelector(element);
 }
 
-const minutes = select('#minutes');
-const minutes1 = select('#minutes1');
-const seconds = select('#seconds');
-const seconds1 = select('#seconds1');
+const minutes = select("#minutes");
+const minutes1 = select("#minutes1");
+const seconds = select("#seconds");
+const seconds1 = select("#seconds1");
 
-
-function movement(){
-  let moveScore=document.getElementById('moves');
-  moveScore.innerHTML=move;
+function movement() {
+  let moveScore = document.getElementById("moves");
+  moveScore.innerHTML = move;
 }
 
 // function to start time counter
@@ -45,7 +44,6 @@ function timer() {
   if (secondsCount < 10) {
     seconds.innerHTML = `0${secondsCount}`;
     seconds1.innerHTML = `0${secondsCount}`;
-    
   } else {
     seconds.innerHTML = secondsCount;
   }
@@ -53,13 +51,12 @@ function timer() {
   minutes1.innerHTML = minuteCount;
 }
 
-
 function turnCard() {
   startTimer();
   if (doneCard) return;
   if (this === firstCard) return;
 
-  this.classList.add('turn');
+  this.classList.add("turn");
 
   if (!turned) {
     turned = true;
@@ -83,43 +80,39 @@ function isMached() {
 
   isMatch ? disableCards() : unturnCards();
   move++;
-  
 }
 
 function disableCards() {
-  firstCard.removeEventListener('click', turnCard);
-  secondCard.removeEventListener('click', turnCard);
+  firstCard.removeEventListener("click", turnCard);
+  secondCard.removeEventListener("click", turnCard);
 
   resetBoard();
   pairs++;
-  if(pairs===8){
-    let win=document.getElementById('welcome-screen');
-    
-    score.innerHTML='wel done';
-    win.classList.add('visible');
-    // innerHTML = ` ${minuteCount} min(s) ${secondsCount} secs`;
-    if(move<22){
-      score.innerHTML=`well done with ${move} moves`;
-    }else if(move<30){
-      score.innerHTML=`well done,with ${move} moves `;
-      document.querySelector('.star3').classList.remove('gold-star');
+  if (pairs === 8) {
+    let win = document.getElementById("welcome-screen");
 
-    } else{
-      score.innerHTML=`well done,with ${move} moves `;
-      document.querySelector('.star3').classList.remove('gold-star');
-      document.querySelector('.star2').classList.remove('gold-star');
+    score.innerHTML = "wel done";
+    win.classList.add("visible");
+    // innerHTML = ` ${minuteCount} min(s) ${secondsCount} secs`;
+    if (move < 22) {
+      score.innerHTML = `well done with ${move} moves`;
+    } else if (move < 30) {
+      score.innerHTML = `well done,with ${move} moves `;
+      document.querySelector(".star3").classList.remove("gold-star");
+    } else {
+      score.innerHTML = `well done,with ${move} moves `;
+      document.querySelector(".star3").classList.remove("gold-star");
+      document.querySelector(".star2").classList.remove("gold-star");
     }
-    
   }
-  
 }
 
 function unturnCards() {
   doneCard = true;
 
   setTimeout(() => {
-    firstCard.classList.remove('turn');
-    secondCard.classList.remove('turn');
+    firstCard.classList.remove("turn");
+    secondCard.classList.remove("turn");
 
     resetBoard();
   }, 1000);
@@ -137,7 +130,7 @@ function resetBoard() {
   });
 })();
 
-cards.forEach(card => card.addEventListener('click', turnCard));
-function resetGame(){
+cards.forEach(card => card.addEventListener("click", turnCard));
+function resetGame() {
   location.reload();
 }
